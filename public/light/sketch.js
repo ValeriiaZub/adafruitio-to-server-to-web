9b87bb5338aecf0626b16702ec11941412e90d6a
ValeriiaZub <45237320+ValeriiaZub@users.noreply.github.com>
2021-02-14 20:29:29 -0500

    Fixed url linking

--- public/light/sketch.js
+++ public/light/sketch.js
@@ -14,7 +14,7 @@ let socket;
 function setup() {
 
      //method connects user to the server over web sockets 
-     socket = io();
+     socket = io("/light");
      //The infomation recieved over web sockets with
      //the channel name "update" -> run this code
      socket.on('update', ({data}) => {
